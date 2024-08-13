5674856aec53d62480fa7f90cef32505f97860bb
aparna yendru <97721064+aparnayendru@users.noreply.github.com>
2024-08-13 21:10:01 +0530

    Update Shop.jsx

--- mern-client/src/shop/Shop.jsx
+++ mern-client/src/shop/Shop.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import  {useEffect, useState} from 'react'
 import { Card } from "flowbite-react";
 
 const Shop = () => {
@@ -14,7 +14,7 @@ const Shop = () => {
       
       <div className='grid gap-8 my-12 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 grid-cols-1'>
         {
-          books.map(book=> <Card
+          books.map(book=> <Card key={book.id}
           >
             <img src={book.imageURL} alt="" className='h-96'/>
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
