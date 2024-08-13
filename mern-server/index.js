3a26707ee9fff1b47e699a4498bdd67175fc0095
aparna <aparnayendru@gmail.com>
2024-08-13 20:14:46 +0530

    Every thing is back to original in the mern-server

--- mern-server/index.js
+++ mern-server/index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-const app = express();
+const express = require('express')
+const app = express()
 const port = process.env.PORT || 5000;
 
-// Middlewares
-app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST'],
-  credentials: true
-}));
+//middlewares
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../mern-client/dist'))); // Adjusted path
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-// MongoDB configuration
+
+//mongodb configuration
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://mern-book-store:yMEVpZGi03QwdxdQ@cluster0.lqxsuqb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -29,79 +29,77 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //creat a collection of document
     const bookCollections = client.db("BookInventory").collection("books");
+    //insert a book to the database using post method
+    app.post("/upload-book",async(req,res) =>{
+        const data = req.body;
+        const result = await bookCollections.insertOne(data);
+        res.send(result);
+    })
 
-    // This should be at the top before other routes
-    app.get('/hello', (req, res) => {
-      res.send('Hello world!');
-    });
+    //get all books fromthe database
+    /*app.get("/all-books", async(req,res)=>{
+        const books = bookCollections.find();
+        const result = await books.toArray();
+        res.send(result);
+    })*/
 
-    // Other routes go here
+    //update a book data : patch or post update method
+    app.patch("/book/:id",async(req, res)=>{
+        const id = req.params.id;
+        const updateBookData = req.body;
 
+        const filter ={_id: new ObjectId(id)};
+        const updateDoc = {
+            $set:{
+                ...updateBookData
+            },
+        }
+        const options = {upsert: true};
+        //update
+        const result = await bookCollections.updateOne(filter,updateDoc, options);
+        res.send(result);
+    })
 
-    // Handles all other routes by sending the React app
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../mern-client/dist', 'index.html'));
-    });
+    //delete a book data
+    app.delete("/book/:id",async(req, res)=>{
+        const id = req.params.id;
+        const filter ={_id: new ObjectId(id)};
+        const result = await bookCollections.deleteOne(filter);
+        res.send(result);
+    })
 
-    // Insert a book to the database using POST method
-    app.post("/upload-book", async (req, res) => {
-      const data = req.body;
-      const result = await bookCollections.insertOne(data);
-      res.send(result);
-    });
+    //find by category 
+    app.get("/all-books", async(req, res)=>{
+        let query = {};
+        if(req.query?.category){
+            query = {category: req.query.category}
+        }
+        const result = await bookCollections.find(query).toArray();
+        res.send(result);
+    })
 
-    // Update a book data
-    app.patch("/book/:id", async (req, res) => {
+    //to get a single book data
+    app.get("/book/:id", async(req,res)=>{
       const id = req.params.id;
-      const updateBookData = req.body;
-      const filter = { _id: new ObjectId(id) };
-      const updateDoc = {
-        $set: { ...updateBookData },
-      };
-      const options = { upsert: true };
-      const result = await bookCollections.updateOne(filter, updateDoc, options);
-      res.send(result);
-    });
-
-    // Delete a book data
-    app.delete("/book/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const result = await bookCollections.deleteOne(filter);
-      res.send(result);
-    });
-
-    // Find by category
-    app.get("/all-books", async (req, res) => {
-      let query = {};
-      if (req.query?.category) {
-        query = { category: req.query.category };
-      }
-      const result = await bookCollections.find(query).toArray();
-      res.send(result);
-    });
-
-    // To get a single book data
-    app.get("/book/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
+      const filter = {_id : new ObjectId(id)};
       const result = await bookCollections.findOne(filter);
       res.send(result);
-    });
-
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    //await client.close(); 
   }
 }
-
 run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
